c573f381794b6081ab38d2e943455638a81cfb70
4rip22 <kkruipputra816@gmail.com>
2024-10-24 21:34:35 +0700

    code NoteList

---
+++ catatan-App/components/NoteList.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import NoteItem from "./NoteItem";
+
+function NoteList({notes,onDelete,onArchive}) {  
+    const activeNotes = notes.filter(note => !note.archived);
+    const archivedNotes = notes.filter(note => note.archived);
+
+    return(
+        <div>
+            <h2>Catatan Aktif</h2>
+            {activeNotes.length > 0 ? (
+                <div className="note-list">
+                    {activeNotes.map(note => (
+                        <NoteItem 
+                        key={note.id} 
+                        note={note} 
+                        onDelete={onDelete} 
+                        onArchive={onArchive}
+                        />
+                    ))}
+                </div>
+            ) : <p>Tidak ada catatan aktif</p>}
+            
+            <h2>Catatan Arsip</h2>  
+            {archivedNotes.length > 0 ? (
+                <div className="note-list">
+                    {activeNotes.map(note => (
+                        <NoteItem 
+                        key={note.id} 
+                        note={note} 
+                        onDelete={onDelete} 
+                        onArchive={onArchive}
+                        />
+                    ))}
+                </div>
+            ) : <p>Tidak ada catatan aktif</p>} 
+        </div>
+    )
+}
+export default NoteList;
