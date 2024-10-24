e4a380a84c8a13512d00bd6cc7ff09ce30761d3a
4rip22 <kkruipputra816@gmail.com>
2024-10-24 12:34:09 +0700

    handleTitleChange Code

--- catatan-App/components/NoteInput.jsx
+++ catatan-App/components/NoteInput.jsx
@@ -2,6 +2,20 @@ import React, { useState } from 'react';
 
 function NoteInput({addNote}){
     const [title, setTitle] = useState('')
-    const [content, setBody] = useState('')
+    const [body, setBody] = useState('')
     const [titleLimit,setTitleLimit] = useState(50);
+
+    const handleTitleChange = (event) => {
+        event.preventDefault();
+        addNote({
+            title,
+            body,
+            archived: false,
+            createdAt: new Date().toISOString(),
+        });
+        setTitle('');
+        setBody('');
+        setTitleLimit(50);
+    }
 }
+export default NoteInput;
