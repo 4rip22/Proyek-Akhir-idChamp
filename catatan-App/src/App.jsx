6183bc1b86454f109446041badce15b03d66af65
4rip22 <kkruipputra816@gmail.com>
2024-10-24 22:09:33 +0700

    initialNode code

--- catatan-App/src/App.jsx
+++ catatan-App/src/App.jsx
@@ -3,3 +3,13 @@ import NoteItem from '../components/NoteItem';
 import NoteList from '../components/NoteList';
 import NoteSearch from '../components/NoteSearch';
 import "../styles/style.css";
+
+const initialNodes = [
+    {
+        id: 1,
+        title: "Babel",
+        body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
+        archived: false,
+        createdAt: '2022-04-14T04:27:34.572Z'
+      },
+]
