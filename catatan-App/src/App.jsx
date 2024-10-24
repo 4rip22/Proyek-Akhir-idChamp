2d40705af0646ab3f1dc5af8390c79ee767d638e
4rip22 <kkruipputra816@gmail.com>
2024-10-24 22:00:10 +0700

    commit style.css

---
+++ catatan-App/src/App.jsx
@@ -0,0 +1,5 @@
+import React, {useState} from 'react';
+import NoteItem from '../components/NoteItem';
+import NoteList from '../components/NoteList';
+import NoteSearch from '../components/NoteSearch';
+import "../styles/style.css";
